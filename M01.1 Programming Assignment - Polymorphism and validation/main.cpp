f7f96c035e407564bf751eb0fb40b7b9193b450a
Mwai <bandamwai@gmail.com>
2021-03-25 10:35:30 -0500

    Initial Commit

---
+++ M01.1 Programming Assignment - Polymorphism and validation/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  M01.1 Programming Assignment - Polymorphism and validation
+//
+//  Created by Mwai Banda on 3/25/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
